dd405be09f082c959e77539003652395b0c5e079
sfailla4 <31773608+sfailla4@users.noreply.github.com>
2025-01-12 17:47:06 -0800

    Update pytest2.py

--- pytest2.py
+++ pytest2.py
@@ -9,7 +9,7 @@ per_list = ['1y','1mo','3mo','6mo','2y','5y','10y','ytd','max']
 df = pd.DataFrame()
 
 st.write("Enter a stock ticker to learn more about a company!")
-box = st.text_input('Enter a Stock Ticker Here',key='BX1',value='SNOW')
+box = st.text_input('Enter a Stock Ticker Here',key='BX1',value='SNOW',help='Try IBM, TSLA, CHD, or WWW for example!')
 
 tab_des, tab_mkt_data, tab_instit = st.tabs(['Company Description','Market Data','Institutional Holdings'])
 
